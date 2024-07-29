8ae619c5ce4287b366b9467b3605ad4954d67c18
proge <juha@mollerus.fi>
2024-07-29 22:08:07 +0300

    working on setup

--- src/main.py
+++ src/main.py
@@ -1,5 +1,24 @@
 from textnode import TextNode
 
+import os
+import shutil
+
+
 def main():
-    textNode = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(textNode.text)
+    setup()
+
+def recursive_copy(dir, destination):
+    if dir == None or dir == "":
+        raise Exception("dir is empty or None")
+    
+    if os.path.exists(dir):
+        contents = os.listdir
+        
+
+def setup():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+
+    print(os.path.isfile("static"))
+main()
